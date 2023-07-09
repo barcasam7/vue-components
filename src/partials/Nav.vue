c1f14ecb877e6365b8ce1c6c1c885b31c6900084
Sam Ward <sam@wordnerds.ai>
2023-07-09 14:58:54 +0100

    add mobile menu

--- src/partials/Nav.vue
+++ src/partials/Nav.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { routes as links } from "./../router/router";
+import { ref } from "vue";
 const route = useRoute();
-console.log(links);
+let isOpen = ref<boolean>(false);
+
+const toggleMenu = () => {
+   document.getElementById("bar1")?.classList.toggle("animateBar1");
+   document.getElementById("bar2")?.classList.toggle("animateBar2");
+   document.getElementById("bar3")?.classList.toggle("animateBar3");
+   isOpen.value = !isOpen.value;
+};
 </script>
 <template>
-  <div id="sidebar">
-    <nav class="hide-mobile nav">
-      <ul>
-        <li v-for="(routeLink, index) in links" :key="index">
-          <router-link :class="route.fullPath === routeLink.path ? 'active' : ''" :to="routeLink.path"> {{ routeLink.name }}</router-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+   <div id="sidebar">
+      <nav class="hide-mobile nav">
+         <ul>
+            <li v-for="(routeLink, index) in links" :key="index">
+               <router-link :class="route.fullPath === routeLink.path ? 'active' : ''" :to="routeLink.path"> {{ routeLink.name }}</router-link>
+            </li>
+         </ul>
+      </nav>
+      <div className="hamburger" @click="toggleMenu">
+         <div className="bar" id="bar1"></div>
+         <div className="bar" id="bar2"></div>
+         <div className="bar" id="bar3"></div>
+      </div>
+      <nav :class="isOpen ? 'opened' : ''" class="mobile-nav">
+         <ul>
+            <li v-for="(routeLink, index) in links" :key="index">
+               <router-link :class="route.fullPath === routeLink.path ? 'active' : ''" :to="routeLink.path"> {{ routeLink.name }}</router-link>
+            </li>
+         </ul>
+      </nav>
+   </div>
 </template>
